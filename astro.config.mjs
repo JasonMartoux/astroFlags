eabddf68278276103ef93f9be6504ba6a5b495ec
Jason Martoux <apps+git@martoux.com>
2024-09-27 18:10:34 +0200

    flag play

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import alpinejs from '@astrojs/alpinejs';
+
+import tailwind from '@astrojs/tailwind';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [alpinejs(), tailwind()]
+});
